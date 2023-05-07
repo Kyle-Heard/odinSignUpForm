ebca235352e5a6e1adad952e8a0a30d3920c1a5c
Kyle Heard <KyleLHeard@gmail.com>
2023-05-07 18:22:53 -0400

    Assignment Compelte

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
-console.log('javas');
 
-// Credit Photo by Jarod Lovekamp: https://www.pexels.com/photo/photo-of-a-man-at-the-top-of-a-snowy-mountain-2404444/
+const passwordSet = document.getElementById('userPassword');
+const passwordConfirm = document.getElementById('confirmPassword');
+
+passwordConfirm.addEventListener('input', e => checkPasswordMatch(e));
+passwordSet.addEventListener('input', e => checkPasswordMatch(e));
+
+function checkPasswordMatch(e){
+    console.log(passwordConfirm.value + " " + passwordSet.value);
+    if (passwordSet.value.length <= 0) passwordSet.setCustomValidity("Invalid Field");
+    else if (passwordConfirm.value !== passwordSet.value) passwordSet.setCustomValidity("Invalid Field");
+    else passwordSet.setCustomValidity("");
+}
